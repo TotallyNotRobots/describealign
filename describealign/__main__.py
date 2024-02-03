dbefbb709e820ec1abbce048ba9da2c9f3c47691
linuxdaemon <linuxdaemon.irc@gmail.com>
2024-02-03 22:41:50 +0000

    Restructure source folder

---
+++ describealign/__main__.py
@@ -0,0 +1,6 @@
+from describealign.main import main
+
+# allows the script to be run on its own, rather than through the package, for example:
+# python3 describealign.py video.mp4 audio_desc.mp3
+if __name__ == "__main__":
+    main()
